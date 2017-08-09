c97e8672394992ffdf031dc3dc0c65f4a4564cb8
CristopherJMB <30849202+CristopherJMB@users.noreply.github.com>
2017-08-09 09:26:02 -0500

    Commit

--- app/src/main/java/com/example/hp/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hp/myapplication/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(),"usuario y/o contraseña Incorrectos******",Toast.LENGTH_LONG).show();
                     }
 
+
                     txtusu.setText("");
                     txtpas.setText("");
                     txtusu.findFocus();
